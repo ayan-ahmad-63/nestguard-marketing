ec5a0fc7ec65b0e050f82017080f73572731a698
Ahmad Mujtaba <hanankhawar123@gmail.com>
2026-09-15 10:17:32 +0500

    speed and analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "@/index.css";
 
 export const metadata: Metadata = {
@@ -35,7 +37,11 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
